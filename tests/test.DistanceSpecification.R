80957089dd8518f9fc32f5e28ce9c305cef4692a
Mark Fredrickson <mark.m.fredrickson@gmail.com>
2011-06-22 15:33:41 -0500

    Removing SparseM dependency

--- tests/test.DistanceSpecification.R
+++ tests/test.DistanceSpecification.R
@@ -12,12 +12,6 @@ test_that("Basic types are DistanceSpeficiations", {
   m <- matrix(1, nrow = 2, ncol = 2)
   expect_true(is(m, "DistanceSpecification")) # expect_is fails for S3 classes?
 
-  m.csr <- as.matrix.csr(m)
-  expect_is(m.csr, "DistanceSpecification")
-
-  # make up a optmatch.dlist
-  # odl <- mdist(pr ~ t1 + t2, data = nuclearplants)
-  # expect_is(odl, "DistanceSpecification")
   
 })
 
@@ -38,11 +32,5 @@ test_that("DistSpec => nodes and arcs", {
   expect_equal(unique(m.result$treatment), as.factor(c("D", "E")))
   expect_equal(unique(m.result$control), as.factor(c("A", "B", "C")))
 
-  # for the sparse representation, we use 0 to notate unmatchable
-  # SparseM does not appear to support row names...
-  m.csr <- as.matrix.csr(matrix(c(1, 0, 1, 2, 2, 0), nrow = 2, ncol = 3))
-
-  m.csr.result <- prepareMatching(m.csr)
-  expect_equal(dim(m.csr.result), c(4,3))
 
 })
