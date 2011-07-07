0f7d2827ee0b9aa0fd8cf9a6763d533c55134ce7
Mark Fredrickson <mark.m.fredrickson@gmail.com>
2011-07-07 12:34:05 -0500

    mahal.dist => mdist

--- tests/fullmatch.R
+++ tests/fullmatch.R
@@ -2,5 +2,6 @@ require('optmatch')
 data(plantdist)
 fullmatch(plantdist<10)
 data(nuclearplants)
-mhd2 <- mahal.dist(~date+cum.n, nuclearplants, pr~pt)
+mhd2 <- mdist(pr ~ date + cum.n, data = nuclearplants, 
+              excludes = exactMatch(pr ~ pt, data = nuclearplants))
 fullmatch(mhd2 < 1)
