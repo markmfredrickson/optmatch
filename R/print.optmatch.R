6bf51c92ba9a457249f1d4a9c4c242053aeda34c
Mark Fredrickson <mark.m.fredrickson@gmail.com>
2011-07-12 17:23:16 -0500

    Option to the optmatch.print method to group units
    Example using data from tests/fullmatch.R:
    
    > aaa <- fullmatch(mhd2 < 1)
    > aaa
      H   I   A   J   B   K   L   M   C   N   O   P   Q   R   S   T   U   D   V   E
    0.7 0.7 0.1 0.7 0.2 0.7 0.7 0.7 0.3 0.7 0.7 0.7 0.7 0.7 0.7 0.7 0.6 0.4 0.5 0.5
      W   F   X   G   Y   Z   d   e   f   a   b   c
    0.4 0.6 0.3 0.7 0.2 0.1 1.3 1.2 1.1 1.1 1.2 1.3
    > print(aaa, grouped = T)
     Group                                  Members
       0.1                                     A, Z
       0.2                                     B, Y
       0.3                                     C, X
       0.4                                     D, W
       0.5                                     V, E
       0.6                                     U, F
       0.7 H, I, J, K, L, M, N, O, P, Q, R, S, T, G
       1.1                                     f, a
       1.2                                     e, b
       1.3                                     d, c
    >

--- R/print.optmatch.R
+++ R/print.optmatch.R
@@ -1,12 +1,19 @@
-print.optmatch <- function(x, quote=FALSE, ...)
+print.optmatch <- function(x, quote = FALSE, grouped = FALSE, ...)
 {
-   if (length(x) <= 0)
-      cat("factor(0)\n")
-   else 
-   {
-   rv <- as.character(x)
-   names(rv) <- names(x)
-   print(rv, quote=FALSE, ...)
-   }
-   invisible(x)
+  if (length(x) <= 0) {
+    cat("factor(0)\n")
+  }
+  else 
+  {
+    if (grouped) {
+      tmp <- aggregate(names(x), by = list(x), FUN = as.character)
+      colnames(tmp) <- c("Group", "Members")
+      print(tmp, quote = quote, row.names = FALSE, ...)
+    } else {
+      rv <- as.character(x)
+      names(rv) <- names(x)
+      print(rv, quote = quote, ...)
+    }
+  }
+  invisible(x)
 }
