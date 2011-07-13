0efb80076194a70dc62dc437fa0aa31fcf9aed20
Mark Fredrickson <mark.m.fredrickson@gmail.com>
2011-07-13 12:03:07 -0500

    updated fullmatch output tests, results same

--- tests/fullmatch.R
+++ tests/fullmatch.R
@@ -4,4 +4,10 @@ fullmatch(plantdist<10)
 data(nuclearplants)
 mhd2 <- mdist(pr ~ date + cum.n, data = nuclearplants, 
               exclusions = exactMatch(pr ~ pt, data = nuclearplants))
-fullmatch(caliper(1, mhd2))
+# the previous version of optmatch used fullmatch(mhd2 < 1)
+# this is the equivalent using an ISM (logical operators treat them as numeric
+# vectors)
+mhd2[mhd2 < 1] <- 1
+mhd2[mhd2 >= 1] <- 0
+fullmatch(mhd2)
+
