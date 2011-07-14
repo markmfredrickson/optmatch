f70e4176e1cc383ebdc1ba6f12fc8e6efa63b23e
Mark Fredrickson <mark.m.fredrickson@gmail.com>
2011-07-13 20:54:13 -0500

    updating stratStruct, differences to do single matrix vs. list

--- tests/stratumStructure-tests.R
+++ tests/stratumStructure-tests.R
@@ -9,12 +9,14 @@ stratumStructure(plantsfm1)
 
 data(nuclearplants)
 psd <- mdist(glm(pr~.-(pr+cost), family=binomial(),
-                       data=nuclearplants))
+                       data=nuclearplants),
+             standardization.scale = sd) # sd was standard in < 0.7-2
 stratumStructure(fullmatch(psd))
 stratumStructure(fm.psd.cal <- fullmatch(psd/(psd<.25)))
 
 psd2 <- mdist(glm(pr~.-(pr+cost), family=binomial(),
                        data=nuclearplants),
+              standardization.scale = sd,
               exclusions = exactMatch(pr ~ pt, nuclearplants))
 stratumStructure(fullmatch(psd2,min.controls=1,max.controls=1))
 stratumStructure(fullmatch(psd2,min.controls=3,max.controls=3))
