40290e16963a626eea87453ad6426763ebc000fd
Mark Fredrickson <mark.m.fredrickson@gmail.com>
2011-07-13 19:56:01 -0500

    Updating pairmatch tests. May be a bug in #5.
    In short, a failure in a subclass causes an entire match to fail in the
    new version, whereas it it only caused failure in the specific subclass
    in the old verion.

--- tests/pairmatch-tests.R
+++ tests/pairmatch-tests.R
@@ -5,9 +5,14 @@ pairmatch(plantdist, controls=2)
 pairmatch(plantdist/(plantdist<1)) # Matching fails everywhere
 pairmatch(plantdist/(plantdist<5), remove.unmatchables=TRUE) # Matching works after removing plant 'F'
 data(nuclearplants)
+# in both of mdist calls below use sd to maintain backwards compatibility with
+# pscore.dist, which used sd by default. mdist has used mad as the std. scale
+# since it was added to the package, so the use of mdist should be consistent
+# for users going forward.
 psm <- glm(pr~.-(pr+cost), family=binomial(), data=nuclearplants)
-psd <- mdist(psm)
+psd <- mdist(psm, standardization.scale = sd)
 pairmatch(psd, controls=2)
-pairmatch(caliper(mdist(psm, exclusions = exactMatch(pr ~ pt, data =
-nuclearplants)), width=2)) # Fails in subclass '1'
+pairmatch(caliper(mdist(psm, standardization.scale = sd,
+  exclusions = exactMatch(pr ~ pt, data =
+  nuclearplants)), width=2)) # Fails in subclass '1'
 
