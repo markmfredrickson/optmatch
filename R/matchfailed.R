cdbd76a3ec69e6ccdc6b6ea8da66e46bc8fa6f4b
Mark Fredrickson <mark.m.fredrickson@gmail.com>
2011-11-25 20:06:29 -0600

    failed matches indicated with a true NA now

--- R/matchfailed.R
+++ R/matchfailed.R
@@ -1,8 +1,2 @@
-matchfailed <- function(matchobject) {
-ans <- logical(length(matchobject))
-if (!inherits(matchobject,'optmatch')) {
-  warning('argument of matchfailed() not of class \'optmatch\'', call.=FALSE)
-}
-ans[grep("[.]NA$", as.character(matchobject))] <- TRUE
-ans
-}
+# this should be deprecated...
+matchfailed <- is.na
