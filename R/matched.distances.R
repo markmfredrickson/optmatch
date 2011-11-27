7a2cebf2c849852b08c0c13dcdf1b854377ffefc
Mark Fredrickson <mark.m.fredrickson@gmail.com>
2011-11-27 14:04:52 -0600

    more on names in matched distances

--- R/matched.distances.R
+++ R/matched.distances.R
@@ -7,6 +7,18 @@ matched.distances <- function(matchobj, distance, preserve.unit.names = FALSE)
       rs <- rownames(distance) %in% x
       cs <- colnames(distance) %in% x
       tmp <- as.vector(subset(distance, subset = rs, select = cs, drop = !preserve.unit.names))
+
+      if (sum(rs) > sum(cs)) {
+        names(tmp) <- rownames(distance)[rs]  
+      }
+
+      if (sum(cs) > sum(rs)) {
+        names(tmp) <- colnames(distance)[cs]
+      }
+
+      if (sum(cs) == sum(rs)) {
+        names(tmp) <- colnames(distance)[cs] # either would be correct, and we don't make any guarantees about if we report t or c
+      }
       return(tmp)
   })
 
--- inst/tests/test.Optmatch.R
+++ inst/tests/test.Optmatch.R
@@ -8,7 +8,7 @@ context("Optmatch object")
 test_that("Object creation", {
   dist <- diag(5)
   dimnames(dist) <- list(letters[1:5], letters[6:10])
-  ms <- list(c(a = 1, f = 1, b = 1 , g = 2), c(c = 1, h = 1, d = 2, i = 2, e = 3, j = 3))  
+  ms <- list(c(a = 1, f = 1, b = 2, g = 2), c(c = 1, h = 1, d = 2, i = 2, e = 3, j = 3))  
   res.opt <- makeOptmatch(dist, ms, NULL)
 
   expect_equal(length(res.opt), 10)
@@ -16,16 +16,16 @@ test_that("Object creation", {
   expect_is(res.opt, "optmatch")
   
   # two levels of matches shouldn't be 1.NA, 2.NA, should just be NA
-  ms2 <- list(c(a = 1, b = 2, c = 1 , d = NA), c(e = 1, f = 2, g = 3, h = 1, i = 2, j = NA))
+  ms2 <- list(c(a = 1, f = 1, b = 1, g = NA), c(c = 1, h = 1, d = 2, i = 2, e = 2, j = NA))  
   res.opt2 <- makeOptmatch(dist, ms2, NULL)
-  expect_true(all(is.na(res.opt2[c("d", "j")])))
+  expect_true(all(is.na(res.opt2[c("g", "j")])))
 
 })
 
 test_that("Object subsetting", {
   dist <- diag(5)
   dimnames(dist) <- list(letters[1:5], letters[6:10])
-  ms <- list(c(a = 1, f = 1, b = 1 , g = 2), c(c = 1, h = 1, d = 2, i = 2, e = 3, j = 3))  
+  ms <- list(c(a = 1, f = 1, b = 2, g = 2), c(c = 1, h = 1, d = 2, i = 2, e = 3, j = 3))  
   res.opt <- makeOptmatch(dist, ms, NULL)
 
   expect_equal(names(res.opt[1:4]), c("a", "f", "b", "g"))
@@ -58,5 +58,19 @@ test_that("Matched distances", {
 
   res.names.i <- matched.distances(dist.match, dist.i, preserve.unit.names = TRUE)
   expect_equal(names(res.names.i), c("1.1", "1.2", "2.1", "2.2", "2.3"))
+
+  # matches with more than one item in a strata
+  match.multiple <- as.factor(c(1.1,1.1,NA,1.1,2.1,2.1,2.2,2.2,2.3,2.3))
+  names(match.multiple) <- c("a","f","b","g","c","h","d","i","e","j")
+  class(match.multiple) <- c("optmatch", "factor")
+
+  dist.multiple <- dist
+  dist.multiple["a", "g"] <- 99
+  
+  res.multiple <- matched.distances(match.multiple, dist.multiple, preserve.unit.names = T)
+  expect_equal(length(res.multiple), 4) # 4 matches, four item list
+  expect_equal(as.vector(unlist(res.multiple)), c(1, 99, 3, 4, 5))
+  expect_equal(as.vector(unlist(sapply(res.multiple, names))), c("f", "g", "h", "i", "j"))
+
 
 })
