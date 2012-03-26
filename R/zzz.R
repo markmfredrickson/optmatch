38936f4389de283003a087a8a91a44ddbddfc0dd
Ben <bbh@umich.edu>
2012-03-26 17:26:04 -0300

    Another adjustment to the startup message: edited down some more and added MF to credits.

--- R/zzz.R
+++ R/zzz.R
@@ -1,16 +1,12 @@
 .onAttach <- function(lib, pkg) {   
 packageStartupMessage(paste(
-"You're loading optmatch, by Ben Hansen, a package for flexible\n",
-"and optimal matching.  Important license information:\n",
+"You're loading optmatch, by B. Hansen and M. Fredrickson.\n",
 "The optmatch package makes essential use of D. P. Bertsekas\n",
 "and P. Tseng\'s RELAX-IV algorithm and code, as well as\n" ,
-"Bertsekas\' AUCTION algorithm and code.\n",
-"Bertsekas and Tseng freely permit their software to be used for\n",
-"research purposes, but non-research uses, including the use of it\n",
+"Bertsekas\' AUCTION algorithm and code.  Using the software\n",
 "to \'satisfy in any part commercial delivery requirements to\n",
-"government or industry,\' require a special agreement with them.\n",
-"By extension, this requirement applies to any use of the \n",
-"fullmatch() function. For more information, enter\n",
+"government or industry\' requires a special agreement with\n",
+"Dr. Bertsekas. For more information, enter\n",
 "relaxinfo() at the command line.\n"
 ))
 } 
