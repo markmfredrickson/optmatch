167d2c4f5fec4466da234c825201cded85e7777c
Ben <bbh@umich.edu>
2012-03-26 17:15:53 -0300

    Edited down startup message by 1 sentence.

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,3 @@
-
 .onAttach <- function(lib, pkg) {   
 packageStartupMessage(paste(
 "You're loading optmatch, by Ben Hansen, a package for flexible\n",
@@ -11,8 +10,7 @@ packageStartupMessage(paste(
 "to \'satisfy in any part commercial delivery requirements to\n",
 "government or industry,\' require a special agreement with them.\n",
 "By extension, this requirement applies to any use of the \n",
-"fullmatch() function. (If you are using another package that has\n",
-"loaded optmatch, then you will probably be using fullmatch indirectly.)\n",
-"For more information, enter relaxinfo() at the command line\n"
+"fullmatch() function. For more information, enter\n",
+"relaxinfo() at the command line.\n"
 ))
 } 
