77c027568c54ccb09531345ea5899d031097fe21
Mark Fredrickson <mark.m.fredrickson@gmail.com>
2012-08-15 18:12:25 -0500

    works for all cases except missing data in glms, #12

--- inst/tests/test.RItools.R
+++ inst/tests/test.RItools.R
@@ -9,34 +9,43 @@ test_that("Summary function adds RItools info", {
 
   ### Testing ways to create glm objects and send them to RItools for balance tests
 
-  test.data <- data.frame(Z = rep(c(0,1),10), X = c(1,2,NA,3,4,NA,5,NA,9,10))
+  test.data <- data.frame(Z = rep(c(0,1),10), X = 1:10)
+  test.data.na <- test.data ; test.data.na$X[c(3,6,8)] <- NA
 
   ## Method 1: passing a matrix/data.frame or similar
 
   # fill.NAs is a layer of indirection between the glm object and RItools
   # putting th test here because this could happen for other ways to create glms, not just fill.NAs
-  test.df <- glm(fill.NAs(Z ~ X, data = test.data), family = binomial)
+  test.df <- glm(fill.NAs(Z ~ sin(X), data = test.data.na), family = binomial)
 
-  test.pm <- pairmatch(mdist(test.df))
+  match.df <- pairmatch(mdist(test.df))
 
   # may cause an error if the function can't get at the proper data that was used to create the glm object
-  res <- summary(test.pm, test.df)
+  res <- summary(match.df, test.df)
 
   expect_true(!is.null(res$balance))
 
   ## Method 2: using a formula and a data argument
   
-  test.fmla <- glm(Z ~ X, data = test.data, family = binomial)
-  test.pm <- pairmatch(mdist(test.fmla))
+  test.fmla <- glm(Z ~ sin(X), data = test.data, family = binomial)
+  match.fmla <- pairmatch(mdist(test.fmla))
 
-  res <- summary(test.pm, test.fmla)
+  res <- summary(match.fmla, test.fmla)
   expect_true(!is.null(res$balance))
 
   ## Method 3: Using enviornment
-  test.env <- with(test.data, glm(Z ~ X, family = binomial))
-  test.pm <- pairmatch(mdist(test.env))
+  test.env <- with(test.data, glm(Z ~ sin(X), family = binomial))
+  match.env <- pairmatch(mdist(test.env))
 
-  res <- summary(test.pm, test.env)
+  res <- summary(match.env, test.env)
+  expect_true(!is.null(res$balance))
+
+  ## Now trying Method 2 with mising values
+
+  test.fna <- glm(Z ~ sin(X), data = test.data.na, family = binomial)
+  match.fna <- pairmatch(mdist(test.fna))
+
+  res <- summary(match.fna, test.fna)
   expect_true(!is.null(res$balance))
   
 })
