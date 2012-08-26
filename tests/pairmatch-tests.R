f51eadf687aac14e5d3e5af8f244e676e2994a9c
Mark Fredrickson <mark.m.fredrickson@gmail.com>
2012-08-25 20:53:37 -0500

    check: fixing warnngs in pairmatch tests

--- tests/pairmatch-tests.R
+++ tests/pairmatch-tests.R
@@ -1,9 +1,12 @@
 require('optmatch')
+
 data(plantdist)
+# warnings about lacking data argument are expected
 pairmatch(plantdist)
 pairmatch(plantdist, controls=2)
 ## Not run: pairmatch(plantdist + caliper(plantdist, 1)) # Matching fails everywhere
 pairmatch(plantdist + caliper(plantdist, 5, compare = `<`), remove.unmatchables=TRUE) # Matching works after removing plant 'F'
+
 data(nuclearplants)
 # in both of mdist calls below use sd to maintain backwards compatibility with
 # pscore.dist, which used sd by default. mdist has used mad as the std. scale
@@ -11,7 +14,7 @@ data(nuclearplants)
 # for users going forward.
 psm <- glm(pr~.-(pr+cost), family=binomial(), data=nuclearplants)
 psd <- mdist(psm, standardization.scale = sd)
-pairmatch(psd, controls=2)
+pairmatch(psd, controls=2, data = nuclearplants)
 
 # Also not run: again an error would be thrown (which R CMD CHECK does not like)
 # pairmatch(caliper(mdist(psm, standardization.scale = sd,
