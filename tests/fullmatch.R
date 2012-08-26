b79027ee988f6a4d3e43e2c8e505c857c3590f81
Mark Fredrickson <mark.m.fredrickson@gmail.com>
2012-08-25 20:18:29 -0500

    check: updating fullmatch Rout (removing warnings)

--- tests/fullmatch.R
+++ tests/fullmatch.R
@@ -1,6 +1,8 @@
 require('optmatch')
 data(plantdist)
+# this will give a warning about not having data order
 fullmatch(1 * (plantdist < 10)) # make plantdist < 10 numeric, not logical
+
 data(nuclearplants)
 mhd2 <- mdist(pr ~ date + cum.n, data = nuclearplants, 
               exclusions = exactMatch(pr ~ pt, data = nuclearplants))
@@ -9,5 +11,5 @@ mhd2 <- mdist(pr ~ date + cum.n, data = nuclearplants,
 # vectors)
 mhd2[mhd2 < 1] <- 1
 mhd2[mhd2 >= 1] <- 0
-fullmatch(mhd2)
+fullmatch(mhd2, data = nuclearplants)
 
