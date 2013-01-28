ffadf02424af3c8deaf472c8a18367693c069503
Mark Fredrickson <mark.m.fredrickson@gmail.com>
2013-01-28 17:31:38 -0600

    moved all distance tests to the distance.R file

--- vignettes/performance/distance.R
+++ vignettes/performance/distance.R
@@ -14,10 +14,23 @@ gc()
 benchmark.sparse.within <- profr(result.sparse.within <- match_on(x =
 predicted, z = DATA$Z, within = exactMatch(Z ~ I(X3 == "a" | X3 == "b"), data = DATA)))
 
+gc()
+benchmark.formula <- profr(result.formula <- match_on(x =
+  Z ~ X1 + X2 + X3, data = DATA))
+
+gc()
+benchmark.glm <- profr(result.glm <- match_on(x =
+  glm(Z ~ X1 + X2 + X3, data = DATA)))
+
+
 save(file = "distance.rda",
   result.dense,
   benchmark.dense,
   result.sparse.caliper,
   benchmark.sparse.caliper,
   result.sparse.within,
-  benchmark.sparse.within)
+  benchmark.sparse.within,
+  result.formula,
+  benchmark.formula,
+  result.glm,
+  benchmark.glm)
