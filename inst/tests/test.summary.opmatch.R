6aee603e2072663179fd57061ed23205031438ba
Mark M. Fredrickson <mark.m.fredrickson@gmail.com>
2013-06-10 12:36:44 -0500

    updating tests to new matching.failed format

--- inst/tests/test.summary.opmatch.R
+++ inst/tests/test.summary.opmatch.R
@@ -8,18 +8,18 @@ test_that("Failing subgroups", {
   # good case:
   m <- matrix(1, nrow = 3, ncol = 4, dimnames = list(letters[1:3], LETTERS[23:26]))
   res.good <- summary(fullmatch(m))
-  expect_true(all(res.good$matching.failed == FALSE))
+  expect_true(all(res.good$matching.failed == 0))
 
   # good case, but one unit unmatched
   m[, 4] <- Inf
 
   res.one.unmatched <- summary(fullmatch(m))
-  expect_true(all(res.one.unmatched$matching.failed == FALSE))
+  expect_true(all(res.one.unmatched$matching.failed == 0))
 
   # matching fails for all
   f <- matrix(Inf, nrow = 3, ncol = 4, dimnames = list(letters[1:3], LETTERS[23:26]))
   res.all.fail <- summary(fullmatch(f))
-  expect_true(all(res.all.fail$matching.failed == TRUE))
+  expect_true(all(res.all.fail$matching.failed > 0))
 
   # blocked good, 2 2x2 groups
   b <- rep(c("A", "B"), each = 8)
@@ -28,20 +28,20 @@ test_that("Failing subgroups", {
   em <- exactMatch(z ~ b)
 
   res.bg <- summary(fullmatch(em))
-  expect_true(all(res.bg$matching.failed == FALSE))
+  expect_true(all(res.bg$matching.failed == 0))
 
   # blocked group as above but with one un-matched unit
   x <- matrix(0, nrow = 8, ncol = 8, dimnames = list(letters[c(2,4,6,8,10,12,14,16)],
                                                      letters[c(1,3,5,7,9,11,13,15)]))
   x[, "a"] <- Inf
   res.b.one.unmatched <- summary(fullmatch(em + x))
-  expect_true(all(res.b.one.unmatched$matching.failed == FALSE))
+  expect_true(all(res.b.one.unmatched$matching.failed == 0))
 
   # now mock up a match in which one group failed (and there is also one unmatched unit)
   tmp <- fullmatch(em + x)
   tmp[c(letters[9:16])] <- NA
   res.b.subgrp.fail <- summary(tmp)
-  expect_equal(sum(res.b.subgrp.fail$matching.failed), 4)
+  expect_equal(as.vector(res.b.subgrp.fail$matching.failed), c(0,8))
 })
   
 
