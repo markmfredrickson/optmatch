cf2aeaf4cd01f28e95c9bf4bf2e408967b1e15f7
Josh Buckner <bucknerj@umich.edu>
2013-07-26 08:35:13 -0400

    made hash map into persistent str to pos map

--- src/z.c
+++ src/z.c
@@ -56,42 +56,63 @@ int digits(int n) {
   return 1;
 }
 
-void names_to_indexes(SEXP names, SEXP index_names, int * out_index) {
+typedef struct map {
+  struct hsearch_data * hash_tab;
+  ENTRY * entries;
+  size_t n_entries;
+} MAP;
+
+MAP * create_map(SEXP strs) {
+
+  // TODO: check to see that strs is a character vector
+
   int
-    n_index = length(index_names),
-    n_names = length(names),
-    n_hash = ceil((4.0 * (double) n_names) / 3.0);
+    n_strs = length(strs),
+    n_map = ceil((4.0 * (double) n_strs) / 3.0);
+  // hash must be >= 20% empty for efficient look up
 
-  struct hsearch_data * htab = Calloc(n_hash, struct hsearch_data);
+  MAP * strpos = Calloc(1, MAP);
+  strpos->hash_tab = Calloc(n_map, struct hsearch_data);
+  if( 0 == hcreate_r(n_map, strpos->hash_tab) )
+    error("In create_strpos: Failed to create hash table. Out of memory?");
 
-  if( 0 == hcreate_r(n_hash, htab) )
-    error("in names_to_indexes: failed to create hash to index data row names\npossibly out of memory\n");
+  strpos->entries = Calloc(n_strs, ENTRY);
+  strpos->n_entries = n_strs;
 
-  ENTRY
-    to_find, * found,
-    * entries = Calloc(n_index, ENTRY);
-
-  for(int i = 0; i < n_names; i++) {
-    entries[i].key = (char *) CHAR(STRING_ELT(names, i));
-    entries[i].data = Calloc(1 + digits(i), char);
-    sprintf(entries[i].data, "%d", i);
-    if( 0 == hsearch_r(entries[i], ENTER, &found, htab) )
-	error("in names_to_indexes: unknown error on inserting key into hash table; table might be full\n");
+  ENTRY * inserted;
+  for(int i = 0; i < n_strs; i++) {
+    strpos->entries[i].key = (char *) CHAR(STRING_ELT(strs, i));
+    strpos->entries[i].data = Calloc(1 + digits(i), char);
+    sprintf(strpos->entries[i].data, "%d", i);
+    if( 0 == hsearch_r(strpos->entries[i], ENTER, &inserted, strpos->hash_tab) )
+	error("In create_strpos: Can't insert key. Table full?");
   }
+  return strpos;
+}
 
-  for(int i = 0; i < n_index; i++) {
-    to_find.key = (char *) CHAR(STRING_ELT(index_names, i));
-    if( 0 == hsearch_r(to_find, FIND, &found, htab) )
-	error("in names_to_indexes: element of index not found among rownames\n");
-    out_index[i] = (int) strtol(found->data, NULL, 0);
-  }
+void delete_map(MAP * strpos) {
 
-  hdestroy_r(htab);
-  for(int i = 0; i < n_index; i++)
-    Free(entries[i].data);
+  // TODO: check for NULL pointers (strpos members too)
 
-  Free(entries);
-  Free(htab);
+  hdestroy_r(strpos->hash_tab);
+  for(int i = 0; i < strpos->n_entries; i++)
+    Free(strpos->entries[i].data);
+
+  Free(strpos->entries);
+  Free(strpos->hash_tab);
+  Free(strpos);
+}
+
+int get_pos(const char * to_find, MAP * strpos) {
+
+  // TODO: check for NULL pointers (strpos members too)
+
+  ENTRY to_find_e, * found;
+  to_find_e.key = (char *) to_find;
+  if( 0 == hsearch_r(to_find_e, FIND, &found, strpos->hash_tab) )
+    error("In get_pos: String not found.");
+
+  return strtol(found->data, NULL, 0);
 }
 
 SEXP z(SEXP data, SEXP data_row_names, SEXP index, SEXP invScaleMat) {
@@ -99,28 +120,28 @@ SEXP z(SEXP data, SEXP data_row_names, SEXP index, SEXP invScaleMat) {
     nv = nrows(index), n = ncols(data),
     data_rows = nrows(data);
 
-  int
-    va, vb,
-    * index_row_i = Calloc(2 * nv, int);
-
-  names_to_indexes(data_row_names, index, index_row_i);
-
   double * pairDiff = Calloc(nv * n, double);
+
+  MAP * strpos = create_map(data_row_names);
+  // TODO: check for valid strpos
+
+  int va, vb;
   for(int i = 0; i < nv; i++) {
-    va = index_row_i[i];
-    vb = index_row_i[i + nv];
+    va = get_pos(CHAR(STRING_ELT(index, i)), strpos);
+    vb = get_pos(CHAR(STRING_ELT(index, i + nv)), strpos);
     for(int j = 0; j < n; j++) {
       pairDiff[i + j * nv] = REAL(data)[va + j * data_rows]
 	    - REAL(data)[vb + j * data_rows];
     }
   }
-  Free(index_row_i);
+  delete_map(strpos);
 
   char
     side = 'R', uplo = 'U';
   double
     alpha = 1.0, beta = 0.0,
     * product = Calloc(nv * n, double);
+
   // pairDiff * invScaleMat
   F77_CALL(dsymm)(&side, &uplo, &nv, &n, &alpha, REAL(invScaleMat), &n,
 		  pairDiff, &nv, &beta, product, &nv);
