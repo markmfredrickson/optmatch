96758bc3b261f7115ea77de904dc4ad570a5ea74
Mark M. Fredrickson <mark.m.fredrickson@gmail.com>
2014-08-05 14:27:50 -0500

    starting a distUnion function to join distance specifications

---
+++ R/distUnion.R
@@ -0,0 +1,43 @@
+#' Combine multiple distance specifications into a single distance specification.
+#'
+#' @param ... The distance specifications.
+#' @return An InfinitySparseMatrix object with all treated and control units from the arguments.
+#' @export
+distUnion <- function(...) {
+  arglst <- list(...)
+
+  if (!all(sapply(arglst, validDistanceSpecification))) {
+    stop("All arguments must be valid distance specifications")
+  }
+
+  isms <- lapply(arglst, as.InfinitySparseMatrix) 
+
+  treateds <- lapply(isms, function(i) { i@rownames })
+  controls <- lapply(isms, function(i) { i@colnames })
+
+  utreated  <- unique(unlist(treateds))
+  ucontrols <- unique(unlist(controls))
+
+  tmap <- 1:length(utreated)
+  names(tmap) <- utreated
+
+  cmap <- 1:length(ucontrols)
+  names(cmap) <- ucontrols
+
+  updated.isms <- lapply(isms, function(i) {
+    rnms <- i@rownames[i@rows]
+    cnms <- i@colnames[i@cols]
+    i@rows <- tmap[rnms]
+    i@cols <- cmap[cnms]
+    return(i)
+  })
+
+  unionism <- makeInfinitySparseMatrix(
+      unlist(sapply(updated.isms, function(i) { i@.Data })),
+      unlist(sapply(updated.isms, function(i) { i@cols })),
+      unlist(sapply(updated.isms, function(i) { i@rows })),
+      ucontrols,
+      utreated)
+  
+  return(unionism)
+}
