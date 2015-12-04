4bd9731fb1e2522a1cb42858acaac24e1c01e6d4
Josh Errickson <josh@errickson.net>
2015-12-04 10:06:25 -0500

    When loading optmatch interactively, it will only load exported functions (as if you had used library)
    To load all, run devtools::load_all() in the new R

--- load.R
+++ load.R
@@ -1,7 +1,7 @@
 ## Use devtools to load most recent version
 library(utils)
 library("devtools")
-devtools:::load_all()
+devtools:::load_all(export_all=FALSE)
 
 ## `utils` is loaded first to ensure proper ordering in the search
 ## stack, so that devtool's versions of `help` and `?` mask `utils`.
