b6ba7229f9ba037deca3ea7bab0418f2e77d2098
Josh Errickson <josh@errickson.net>
2015-12-03 21:20:46 -0500

    Moved matched.distances doc in

--- R/matched.distances.R
+++ R/matched.distances.R
@@ -1,3 +1,34 @@
+#' From a match (as produced by \code{pairmatch} or \code{fullmatch})
+#' and a distance, extract the distances of matched units from their
+#' matched counterparts.
+#'
+#' From a match (as produced by \code{pairmatch} or \code{fullmatch})
+#' and a distance, extract the distances of matched units from their
+#' matched counterparts.
+#'
+#' @title Determine distances between matched units
+#' @param matchobj Value of a call to \code{pairmatch} or
+#'   \code{fullmatch}.
+#' @param distance Either a distance matrix or the value of a call to
+#'   or \code{match_on}.
+#' @param preserve.unit.names Logical.  If TRUE, for each matched set
+#'   \code{matched.distances} returns the submatrix of the distance
+#'   matrix corresponding to it; if FALSE, a vector containing the
+#'   distances in that submatrix is returned.
+#' @return A list of numeric vectors (or matrices) of distances, one
+#'   for each matched set.  Note that a matched set with 1 treatment
+#'   and k controls, or with k treatments and 1 control, has k, not
+#'   k+1, distances.
+#'
+#' @author Ben B. Hansen
+#' @examples
+#'
+#' data(plantdist)
+#' plantsfm <- fullmatch(plantdist)
+#' (plantsfm.d <- matched.distances(plantsfm,plantdist,pres=TRUE))
+#' unlist(lapply(plantsfm.d, max))
+#' mean(unlist(plantsfm.d))
+#' @keywords nonparametric
 #' @export
 matched.distances <- function(matchobj, distance, preserve.unit.names = FALSE)
 {
