5b9400929d7e3eba9f3ba68097288d78133490ea
Josh Errickson <josh@errickson.net>
2016-03-23 18:09:07 -0400

    Porting over summary.InfinitySparseMatrix and similar from optmatchExperimental

---
+++ R/summary.ism.R
@@ -0,0 +1,271 @@
+##' Summarize a distance matrix
+##'
+##' Given a distance matrix, return information above it, including
+##' dimension, sparsity information, unmatchable members, summary of
+##' finite distances, and, in the case of
+##' \code{BlockedInfinitySparseMatrix}, block structure.
+##'
+##' The output consists of several pieces.
+##'
+##' \itemize{
+##'  \item{Membership: }{Indicates the dimension of the distance.}
+##'
+##'  \item{Total (in)eligible potential matches: }{A measure of the
+##'   sparsity of the distance. Eligible matches have a finite
+##'   distance between treatment and control members; they could be
+##'   matched. Ineligible matches have \code{Inf} distance and can not
+##'   be matched. A higher number of ineligible matches can speed up
+##'   matching, but runs the risk of less optimal overall matching
+##'   results.}
+##'
+##'  \item{Unmatchable treatment/control members: }{If any
+##'   observations have no eligible matches (e.g. their distance to
+##'   every potential match is \code{Inf}) they are listed here. See
+##'   Value below for details of how to access lists of matchable and
+##'   unmatchable treatment and control members.}
+##'
+##'  \item{Summary of minimum matchable distance per treatment member:
+##'   }{To assist with choosing a caliper, this is a numeric summary
+##'   of the smallest distance per matchable treatment member. If you
+##'   provide a caliper that is less than the maximum value, at least
+##'   one treatment member will become unmatchable.}
+##'
+##'  \item{Block structure: }{For \code{BlockedInfinitySparseMatrix},
+##'   a quick summary of the structure of each individual block. (The
+##'   above will all be across all blocks.) This may indicate which
+##'   blocks, if any, are problematic.}
+##' }
+##'
+##' @param object A \code{InfinitySparseMatrix},
+##'   \code{BlockedInfinitySparseMatrix} or \code{DenseMatrix}.
+##' @param ... Ignored.
+##' @param distanceSummary Default \code{TRUE}. Should a summary of
+##'   minimum distance per treatment member be calculated? May be slow
+##'   on larger data sets.
+##' @param printAllBlocks If \code{object} is a
+##'   \code{BlockedInfinitySparseMatrix}, should summaries of all
+##'   blocks be printed alongside the overall summary? Default
+##'   \code{FALSE}.
+##' @param blockStructure If \code{object} is a
+##'   \code{BlockedInfinitySparseMatrix} and \code{printAllBlocks} is
+##'   false, print a quick summary of each individual block. Default
+##'   \code{TRUE}. If the number of blocks is high, consider
+##'   suppressing this.
+##' @return A named \code{list}. The summary for an
+##'   \code{InfinitySparseMatrix} or \code{DenseMatrix} contains the
+##'   following:
+##'
+##'   \itemize{
+##'
+##'    \item{\code{total}: }{Contains the total number of treatment
+##'     and control members, as well as eligible and ineligible
+##'     matches.}
+##'
+##'    \item{\code{matchable}: }{The names of all treatment and
+##'     control members with at least one eligible match.}
+##'
+##'    \item{\code{unmatchable}: }{The names of all treatment and
+##'     control members with no eligible matches.}
+##'
+##'    \item{\code{distances}: }{The summary of minimum matchable
+##'     distances, if \code{distanceSummary} is \code{TRUE}.}
+##'
+##'   }
+##'
+##'   For \code{BlockedInfinitySparseMatrix}, the named \code{list}
+##'   instead of contains one entry per block, named after each block
+##'   (i.e. the value of the blocking variable) as well as a block
+##'   named 'overall' which contains the summary ignoring blocks. Each
+##'   of these entries contains a \code{list} with entries 'total',
+##'   'matchable', 'unmatchable' and 'distances', as described above.
+##' @export
+##' @name summary.ism
+summary.InfinitySparseMatrix <- function(object, ..., distanceSummary=TRUE) {
+
+  finitedata <- is.finite(object@.Data)
+  rowsfinite <- object@rows[finitedata]
+  colsfinite <- object@cols[finitedata]
+  datafinite <- object@.Data[finitedata]
+
+  mtreat <- 1:dim(object)[1] %in% sort(unique(rowsfinite))
+  mcontrol  <- 1:dim(object)[2] %in% sort(unique(colsfinite))
+
+  if (distanceSummary & length(datafinite)) {
+    distances <- summary(tapply(datafinite, rowsfinite, min))
+  } else {
+    distances <- NULL
+  }
+
+  out <- internal.summary.helper(object, mtreat, mcontrol, distances)
+  attr(out, "ismname") <- deparse(substitute(object))
+
+  class(out) <- "summary.InfinitySparseMatrix"
+  out
+}
+
+##' @export
+##' @rdname summary.ism
+summary.BlockedInfinitySparseMatrix <- function(object, ...,
+                                                distanceSummary=TRUE,
+                                                printAllBlocks=FALSE,
+                                                blockStructure=TRUE) {
+
+  ismname <- deparse(substitute(object))
+
+  out <- lapply(levels(object@groups),
+                function(x) {
+                  thisgroup <- names(object@groups[object@groups == x])
+                  ism <- subset(object,
+                                subset=object@rownames %in% thisgroup,
+                                select=object@colnames %in% thisgroup)
+                  s <- summary(ism, ..., distanceSummary=distanceSummary)
+                  attr(s, "ismname") <- ismname
+                  attr(s, "blockname") <- x
+                  return(s)
+                })
+  names(out) <- levels(object@groups)
+
+  out$overall <- summary.InfinitySparseMatrix(object, ...,
+                                              distanceSummary=distanceSummary)
+
+  attr(out, "ismname") <- ismname
+  attr(out, "blocknames") <- levels(object@groups)
+
+  attr(out$overall, "ismname") <- attr(out, "ismname")
+
+  attr(out, "printAllBlocks") <- printAllBlocks
+  attr(out, "blockStructure") <- blockStructure
+
+  class(out) <- "summary.BlockedInfinitySparseMatrix"
+  return(out)
+}
+
+##' @export
+##' @rdname summary.ism
+summary.DenseMatrix <- function(object, ..., distanceSummary=TRUE) {
+  mtreat <- apply(object, 1, function(x) any(is.finite(x)))
+  mcontrol <- apply(object, 2, function(x) any(is.finite(x)))
+  if (distanceSummary & length(object@.Data[is.finite(object@.Data)])) {
+    distances <- summary(apply(object, 1, min))
+  } else {
+    distances <- NULL
+  }
+
+  out <- internal.summary.helper(object, mtreat, mcontrol, distances)
+
+  attr(out, "ismname") <- deparse(substitute(object))
+
+  class(out) <- "summary.DenseMatrix"
+  out
+}
+
+internal.summary.helper <- function(x,
+                                    matchabletxt,
+                                    matchablectl,
+                                    distances=NULL) {
+  out <- list()
+  d <- dim(x)
+
+  # Size of treatment and control groups
+  out$total$treatment <- d[1]
+  out$total$control <- d[2]
+
+  # Count of eligble and ineligible pairs.
+  out$total$matchable <- Reduce("+", num_eligible_matches(x))
+  out$total$unmatchable <- prod(d) - out$total$matchable
+
+  out$matchable$treatment <- rownames(x)[matchabletxt]
+  out$matchable$control <- colnames(x)[matchablectl]
+  out$unmatchable$treatment <- rownames(x)[!matchabletxt]
+  out$unmatchable$control <- colnames(x)[!matchablectl]
+
+  out$distances <- distances
+  return(out)
+}
+
+##' @export
+print.summary.InfinitySparseMatrix <- function(x, ...) {
+  cat(paste("Membership:", x$total$treatment, "treatment,",
+            x$total$control, "control\n"))
+  cat(paste("Total eligible potential matches:", x$total$matchable,
+            "\n"))
+  cat(paste("Total ineligible potential matches:", x$total$unmatchable,
+            "\n"))
+  cat("\n")
+
+  numunmatch <- sapply(x$unmatchable, length)
+  for (i in 1:2) {
+    if (numunmatch[i] > 0) {
+      cat(paste0(numunmatch[i], " unmatchable ", names(numunmatch)[i],
+                 " member", if(numunmatch[i] > 1) { "s" } , ":\n"))
+      cat("\t")
+      cat(paste(x$unmatchable[[i]][1:min(5, numunmatch[i])],
+                collapse=", "))
+      if (numunmatch[i] > 5) {
+        cat(", ...\n")
+
+        cat(paste0("See summary(", attr(x, "ismname"), ")",
+                   if (!is.null(attr(x, "blockname"))) {
+                     paste0("$`", attr(x, "blockname"), "`")
+                   }, "$unmatchable$",
+                   names(numunmatch)[i], " for a complete list."))
+        }
+      cat("\n\n")
+    }
+  }
+
+  if (!is.null(x$distances) && any(!is.na(x$distances))) {
+    cat("Summary of minimum matchable distance per treatment member:\n")
+    print(x$distances, ...)
+    cat("\n")
+  }
+  return(invisible(x))
+}
+
+##' @export
+print.summary.BlockedInfinitySparseMatrix <- function(x, ...) {
+
+  cat("Summary across all blocks:\n")
+  print(x$overall, ...)
+  blockentries <- names(x) %in% attr(x, "blocknames")
+
+  if (!attr(x, "printAllBlocks")) {
+    if (attr(x, "blockStructure")) {
+      cat("Block structure:\n")
+      blocksummary <- matrix(unlist(lapply(x[blockentries], "[", "total")),
+                             byrow=TRUE, ncol=4)
+      blocksummary <- cbind(sapply(sapply(sapply(x[blockentries], "[", "matchable"), "[", "treatment"), length),
+                            sapply(sapply(sapply(x[blockentries], "[", "matchable"), "[", "control"), length),
+                            sapply(sapply(sapply(x[blockentries], "[", "unmatchable"), "[", "treatment"), length),
+                            sapply(sapply(sapply(x[blockentries], "[", "unmatchable"), "[", "control"), length))
+      rownames(blocksummary) <- paste0("`",attr(x, "blocknames"),"`")
+      colnames(blocksummary) <- c("Matchable Txt",
+                                  "Matchable Ctl",
+                                  "Unmatchable Txt",
+                                  "Unmatchable Ctl")
+      print(blocksummary)
+
+      cat("\n")
+    }
+
+    cat(paste0("To see summaries for individual blocks,",
+               " call for example summary(",
+               attr(x, "ismname"), ")$`",
+               attr(x, "blocknames")[1], "`.\n"))
+  } else {
+    cat("Indiviual blocks:\n\n")
+    for (i in attr(x, "blocknames")) {
+      cat(paste0("`",i,"`\n"))
+      print(x[[i]])
+    }
+  }
+
+  cat("\n")
+  return(invisible(x))
+}
+
+##' @export
+print.summary.DenseMatrix <- function(x, ...) {
+  print.summary.InfinitySparseMatrix(x, ...)
+  return(invisible(x))
+}
---
+++ tests/testthat/test.summary.ism.R
@@ -0,0 +1,166 @@
+context("summary method for ISM and related")
+
+
+test_that("summary for ISM", {
+  set.seed(1)
+  d <- data.frame(z=rep(0:1, each=5),
+                  x=rnorm(10))
+  rownames(d) <- letters[1:10]
+  m1 <- match_on(z ~ x, data=d)
+  sm1 <- summary(m1)
+
+  m2 <- m1 + caliper(m1, width=1)
+  sm2 <- summary(m2)
+  expect_true(is(sm2, "summary.InfinitySparseMatrix"))
+  expect_true(is.list(sm2))
+  expect_equal(attr(sm2, "ismname"), "m2")
+  expect_equal(sm2$total$treatment, 5)
+  expect_equal(sm2$total$control, 5)
+  expect_equal(sm2$total$matchable, 12)
+  expect_equal(sm2$total$unmatchable, 25-12)
+  expect_equal(length(sm2$matchable$treatment), 5)
+  expect_equal(length(sm2$matchable$control), 4)
+  expect_equal(sm2$unmatchable$treatment, character(0))
+  expect_equal(sm2$unmatchable$control, "d")
+  expect_true(is(sm2$distances, "summaryDefault"))
+
+  m3 <- m2
+  m3[1:2] <- Inf
+  sm3 <- summary(m3)
+
+  expect_equal(sm3$total$matchable, 10)
+  expect_equal(sm3$total$unmatchable, 25-10)
+  expect_equal(length(sm3$matchable$treatment), 4)
+  expect_equal(length(sm3$matchable$control), 4)
+  expect_equal(sm3$unmatchable$treatment, "f")
+  expect_equal(sm3$unmatchable$control, "d")
+  expect_true(is(sm3$distances, "summaryDefault"))
+  expect_true(all(is.finite(sm3$distances)))
+
+  m4 <- m1 + caliper(m1, width=.0001)
+  sm4 <- summary(m4)
+  expect_equal(sm4$matchable$treatment, character(0))
+  expect_equal(sm4$matchable$control, character(0))
+  expect_true(is.null(sm4$distances))
+
+  m5 <- m3
+  m5@.Data <- rep(Inf, length(m5))
+  sm5 <- summary(m5)
+  expect_equal(sm5$matchable$treatment, character(0))
+  expect_equal(sm5$matchable$control, character(0))
+  expect_true(is.null(sm5$distances))
+
+
+})
+
+test_that("summary for BlockedISM", {
+  set.seed(1)
+  d <- data.frame(z=rep(0:1, each=5),
+                  x=rnorm(10),
+                  q=rep(c("a", "d"), times=5))
+  rownames(d) <- letters[1:10]
+  m1 <- match_on(z ~ x + strata(q), data=d, caliper=1)
+  sm1 <- summary(m1)
+
+  expect_true(is(sm1, "summary.BlockedInfinitySparseMatrix"))
+  expect_true(is.list(sm1))
+  expect_equal(length(sm1), 3)
+  expect_equal(names(sm1), c("a", "d", "overall"))
+  expect_equal(attr(sm1, "ismname"), "m1")
+  expect_equal(attr(sm1, "blocknames"), c("a", "d"))
+  expect_equal(attr(sm1, "printAllBlocks"), FALSE)
+  expect_equal(attr(sm1, "blockStructure"), TRUE)
+  expect_true(is(sm1[["a"]], "summary.InfinitySparseMatrix"))
+  expect_true(is(sm1[["d"]], "summary.InfinitySparseMatrix"))
+
+  sm2 <- summary(m1, printAllBlocks=TRUE, blockStructure=FALSE)
+  expect_equal(attr(sm2, "printAllBlocks"), TRUE)
+  expect_equal(attr(sm2, "blockStructure"), FALSE)
+
+  expect_true(all.equal(c(5,5,4,21),
+                        unlist(sm1$overall$total),
+                        check.attributes=FALSE))
+
+  # Alternate ways of calling blocks
+  suma1 <- sm1[['a']]
+  suma2 <- sm1$`a`
+  suma3 <- sm1[[1]]
+  expect_identical(suma1, suma2)
+  expect_identical(suma1, suma3)
+
+
+  expect_equal(attr(sm1$`a`, "blockname"), "a")
+  expect_equal(attr(sm1$`d`, "blockname"), "d")
+
+})
+
+
+test_that("summary for DenseMatrix", {
+  set.seed(1)
+  d <- data.frame(z=rep(0:1, each=5),
+                  x=rnorm(10))
+  rownames(d) <- letters[1:10]
+  m1 <- match_on(z ~ x, data=d)
+  sm1 <- summary(m1)
+  expect_true(is(sm1, "summary.DenseMatrix"))
+  expect_true(is.list(sm1))
+  expect_equal(attr(sm1, "ismname"), "m1")
+  expect_equal(sm1$total$treatment, 5)
+  expect_equal(sm1$total$control, 5)
+  expect_equal(sm1$total$matchable, 25)
+  expect_equal(sm1$total$unmatchable, 0)
+  expect_equal(length(sm1$matchable$treatment), 5)
+  expect_equal(length(sm1$matchable$control), 5)
+  expect_equal(sm1$unmatchable$treatment, character(0))
+  expect_equal(sm1$unmatchable$control, character(0))
+  expect_true(is(sm1$distances, "summaryDefault"))
+
+  m2 <- m1
+  m2[1,] <- Inf
+  sm2 <- summary(m2)
+  expect_true(is(sm2, "summary.DenseMatrix"))
+  expect_true(is.list(sm2))
+  expect_equal(sm2$total$treatment, 5)
+  expect_equal(sm2$total$control, 5)
+  expect_equal(sm2$total$matchable, 20)
+  expect_equal(sm2$total$unmatchable, 25-20)
+  expect_equal(length(sm2$matchable$treatment), 4)
+  expect_equal(length(sm2$matchable$control), 5)
+  expect_equal(sm2$unmatchable$treatment, "f")
+  expect_equal(sm2$unmatchable$control, character(0))
+  expect_true(is(sm2$distances, "summaryDefault"))
+})
+
+test_that("distanceSummary suppresses distance", {
+  set.seed(1)
+  d <- data.frame(z=rep(0:1, each=5),
+                  x=rnorm(10),
+                  q=rep(c("a", "d"), times=5))
+  rownames(d) <- letters[1:10]
+  m1 <- match_on(z ~ x, data=d)
+
+  expect_true(!is.null(summary(m1)$distances))
+  expect_true(!is.null(summary(m1, distanceSummary=TRUE)$distances))
+  expect_true(is.null(summary(m1, distanceSummary=FALSE)$distances))
+
+  m2 <- match_on(z ~ x, data=d, caliper=1)
+  expect_true(!is.null(summary(m2)$distances))
+  expect_true(!is.null(summary(m2, distanceSummary=TRUE)$distances))
+  expect_true(is.null(summary(m2, distanceSummary=FALSE)$distances))
+
+  m3 <- match_on(z ~ x + strata(q), data=d, caliper=1)
+  sm3.1 <- summary(m3)
+  expect_true(!is.null(sm3.1$overall$distances))
+  expect_true(!is.null(sm3.1$a$distances))
+  expect_true(!is.null(sm3.1$d$distances))
+
+  sm3.2 <- summary(m3, distanceSummary=TRUE)
+  expect_true(!is.null(sm3.2$overall$distances))
+  expect_true(!is.null(sm3.2$a$distances))
+  expect_true(!is.null(sm3.2$d$distances))
+
+  sm3.3 <- summary(m3, distanceSummary=FALSE)
+  expect_true(is.null(sm3.3$overall$distances))
+  expect_true(is.null(sm3.3$a$distances))
+  expect_true(is.null(sm3.3$d$distances))
+})
