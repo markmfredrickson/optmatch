e194d05e82337884b2993d62d08edfaf500332a3
Josh Errickson <josh@errickson.net>
2016-10-10 15:41:20 -0400

    Fix to failing test on R-devel
    Development changes some of the default table arguments, including using useNA = 'ifany' when calling excludes. Explicilty disabling useNA to avoid it

--- R/summary.optmatch.R
+++ R/summary.optmatch.R
@@ -60,7 +60,9 @@ summary.optmatch <- function(object,
       return(so)
     }
   match.succeed <- tapply(mfd, subprobs, function(x) !all(x))
-  so$matching.failed <- table(subprobs, attr(object, "contrast.group"), exclude=names(match.succeed)[match.succeed])
+  so$matching.failed <- table(subprobs, attr(object, "contrast.group"),
+                              exclude = names(match.succeed)[match.succeed],
+                              useNA = 'no')
   if (prod(dim(so$matching.failed)) == 0) {
     so$matching.failed <- NULL
   } else {
