0ac60ae79dd968d14720304418aa403db8e4e30b
Josh Errickson <josh@errickson.net>
2016-12-22 09:26:57 -0500

    Test to ensure that scores() is actually imputing

--- tests/testthat/test.CBPS.R
+++ tests/testthat/test.CBPS.R
@@ -4,4 +4,13 @@ test_that("internal predict.CBPS function", {
   library(CBPS)
   cpsm <- CBPS(pr ~ date + t1 + t2 + cap + ne + ct + bw + cum.n, ATT = 0, data = nuclearplants)
   expect_true(is(fullmatch(cpsm, data = nuclearplants), "optmatch"))
+
+  # See if scores is properly imputing.
+
+  nuclearplants$t1[c(2,5,10)] <- NA
+  nuclearplants$ne[c(6,25,23)] <- NA
+
+  cpsm <- CBPS(pr ~ date + t1 + t2 + cap + ne + ct + bw + cum.n, ATT = 0, data = nuclearplants)
+  expect_true(length(fullmatch(cpsm, data = nuclearplants)) == nrow(nuclearplants))
+
 })
