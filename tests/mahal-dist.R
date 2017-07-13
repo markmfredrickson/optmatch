7cf8e0eefb5881eccd2818d82f2dd69bb46b237c
Josh Errickson <josh@errickson.net>
2017-07-13 08:55:08 -0400

    Fixed old test

--- tests/mahal-dist.R
+++ tests/mahal-dist.R
@@ -1,18 +1,18 @@
 require('optmatch')
-data(nuclear, package="boot")
+data(nuclear, package = "boot")
 match_on(pr ~ cap, data = nuclear)
 match_on(pr ~ date + cum.n, data = nuclear)
 match_on(pr ~ date + cum.n, data = nuclear, within = exactMatch(pr ~ pt, data = nuclear))
 
 if ( (require(splines)) )
-  match_on(pr ~ ns(date,df=3) + cum.n, data = nuclear)
+  match_on(pr ~ ns(date,df = 3) + cum.n, data = nuclear)
 
 if (require(splines))
-  match_on(pr ~ ns(date,df=3) + cum.n, data = nuclear, exactMatch(pr ~ pt, data = nuclear)
+  match_on(pr ~ ns(date,df = 3) + cum.n, data = nuclear, exactMatch(pr ~ pt, data = nuclear)
 )
-cum.n.q <- cut(nuclear$cum.n, quantile(nuclear$cum.n), include.lowest=TRUE)
+cum.n.q <- cut(nuclear$cum.n, quantile(nuclear$cum.n), include.lowest = TRUE)
 match_on(pr ~ date + cum.n.q, data = nuclear)
-match_on(pr ~ date + cum.n.q, data = nuclear, within = exactMatch(pr~pt, data = nuclear))
+match_on(pr ~ date + cum.n.q, data = nuclear, within = exactMatch(pr ~ pt, data = nuclear))
 
-### should give error, incorrect mode
+### should give error, factors no longer supported
 try(match_on(as.factor(pr) ~ cap, data = nuclear))
