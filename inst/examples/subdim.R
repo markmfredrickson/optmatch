6fa292290ed1a8de09915e8d40fe8396b1424861
Ben B Hansen <bbh@umich.edu>
2017-07-15 00:35:27 -0400

    Update subdim example in light of b767a7c

--- inst/examples/subdim.R
+++ inst/examples/subdim.R
@@ -3,7 +3,7 @@ m1 <- fullmatch(pr ~ t1 + t2, within=em, data=nuclearplants)
 stratumStructure(m1)
 (subdims_em <- subdim(em))
 m2 <- fullmatch(pr ~ t1 + t2, within=em, data=nuclearplants,
-                mean.controls=pmin(1.5, sapply(subdims_em, function(x) x[2]/x[1]))
+                mean.controls=pmin(1.5, subdims_em["controls",] / subdims_em["treatments",])
                 )
 stratumStructure(m2)
                 
