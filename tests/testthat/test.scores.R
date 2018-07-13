39bc59a841a57a667ef1db783d5591c61d0ae27c
Josh Errickson <josh@errickson.net>
2018-07-13 07:37:43 -0400

    Moved bigglm tests to testfile not for CRAN

--- tests/testthat/test.scores.R
+++ tests/testthat/test.scores.R
@@ -211,42 +211,8 @@ test_that("NA imputation", {
 
 })
 
-test_that("scores with bigglm", {
-  # Currently, scores() for bigglm is not supported
-  if (require(biglm)) {
-    n <- 16
-    test.data <- data.frame(Z = rep(0:1, each = n/2),
-                            X1 = rnorm(n, mean = 5),
-                            X2 = rnorm(n, mean = -2, sd = 2))
-
-    bgps <- bigglm(Z ~ X1 + X2, data = test.data, family = binomial())
-    m1 <- lm(Z ~ scores(bgps), data=test.data)
-    m2 <- lm(Z ~ predict(bgps, newdata=test.data), data=test.data)
-
-    expect_equal(fitted(m1), fitted(m2))
-
-    test.data2 <- test.data
-    test.data2$X1[1] <- NA
-
-    # predict's na.action=na.pass is ignored in bigglm, so we'll need to impute
-    # beforehand to get the same results
-    bgps2 <- bigglm(Z ~ X1 + X2, data = test.data2, family = binomial())
-    expect_warning(m3 <- lm(Z ~ scores(bgps2), data=test.data2),
-                   "Imputation and refitting of bigglm objects")
-    m4 <- lm(Z ~ predict(bgps2, newdata=fill.NAs(test.data2)), data=test.data2)
-
-    expect_equal(fitted(m3), fitted(m4))
-
-    suppressWarnings(bgps3 <- bigglm(Z ~ X1*X2, data = test.data2, family = binomial()))
-    fill.test.data <- fill.NAs(Z ~ X1*X2, data=test.data2)
-    expect_warning(m5 <- lm(Z ~ scores(bgps3), data=fill.test.data),
-                   "Imputation and refitting of bigglm objects")
-    m6 <- lm(Z ~ predict(bgps3, newdata=fill.test.data), data=test.data2)
-
-    expect_equal(fitted(m5), fitted(m6))
-  }
-
-})
+# test_that("scores with bigglm", {
+# Moved to test.notforCRAN.R
 
 test_that("scores works with *, interaction and strata", {
   data(nuclearplants)
