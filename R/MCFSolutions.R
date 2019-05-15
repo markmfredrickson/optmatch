b1c06ab2d599e446a3acb71b49c284b867484ad8
Hansen <bbh@umich.edu>
2019-05-15 12:36:40 -0400

    MCFSolutions type, subtypes; basic validity checks.

--- R/MCFSolutions.R
+++ R/MCFSolutions.R
@@ -1,19 +1,80 @@
+### See vignette "dual-MCF-solution" for descriptions of these classes.
+setClass("SubProbInfo", contains="data.frame")
+setValidity("SubProbInfo", function(object){
+    errors <- character(0)
+    if (!all(colnames(object)[1:5]==
+             c("subproblem","hashed_dist","resolution","exceedance","CS_orig_dist")))
+        errors  <- c(errors,
+                     'Cols 1-5 should be:\n\t c("subproblem","hashed_dist","resolution","exceedance","CS_orig_dist")')
+    if (!all(sapply(object[1:2], is.character)==TRUE))
+        errors  <- c(errors,
+                     'Cols 1,2 should have type character.')
+    if (!all(sapply(object[3:4], is.numeric)==TRUE))
+        errors  <- c(errors,
+                     'Cols 3,4 should have type numeric.')
+    if (!is.logical(object[[5]]))
+        errors  <- c(errors,
+                     'Col 5 should have type logical.')
+    if (length(errors)==0) TRUE else errors  
+})
+setClass("NodeInfo", contains="data.frame")
+setValidity("NodeInfo", function(object){
+    errors <- character(0)
+    if (!all(colnames(object)[1:5]==
+             c("name", "price", "kind", "supply", "subproblem")))
+        errors  <- c(errors,
+                     'Cols 1-5 should be:\n\t c("name", "price", "kind", "supply", "subproblem")')
+    if (!all(sapply(object[c(1,3,5)], is.character)==TRUE))
+        errors  <- c(errors,
+                     'Cols 1,3,5 should have type character.')
+    if (!is.double(object[['price']]))
+        errors  <- c(errors,
+                     'Col "price" should have type double.')
+    if (!is.integer(object[['supply']]))
+        errors  <- c(errors,
+                     'Col "supply" should have type integer.')    
+    if ( !all(object[['kind']] %in% c("treatment", "control", "bookkeeping")) )
+        errors  <- c(errors,
+                     "'kind' values other than 'treatment', 'control' or 'bookkeeping'.")
+    if (length(errors)==0) TRUE else errors      
+})
+
+setClass("ArcInfo", representation(matches="data.frame", bookkeeping="data.frame"))
+setValidity("ArcInfo", function(object){
+    errors <- character(0)
+    if (!all(colnames(object@matches)[1:3]==
+             c("subproblem", "treatment",  "control")))
+        errors  <- c(errors,
+                     '@matches cols 1-3 should be:\n\t c("subproblem", "treatment",  "control")')
+### <ADD COLUMN TYPE CHECKS HERE>
+    
+    if (!all(colnames(object@bookkeeping)[1:4]==
+             c("subproblem", "startnode",  "endnode",  "flow")))
+        errors  <- c(errors,
+                     '@bookkeeping cols 1-4 should be:\n\t c("subproblem", "startnode",  "endnode",  "flow")')
+    if (!is.integer(object@bookkeeping[['flow']]))
+        errors  <- c(errors,
+                     '@bookkeeping col "flow" should have type integer.')
+### <ADD FURTHER COLUMN TYPE CHECKS HERE>
+    if (length(errors)==0) TRUE else errors      
+})
+
 setClass("MatchablesInfo", contains="data.frame")
 setValidity("MatchablesInfo", function(object){
     errors <- character(0)
-    for (nm in c('name', 'subproblem', 'kind'))
-    {
-        if ( !(nm %in% colnames(object)) )
-            errors  <- c(errors,
-                         paste0("Need a '", nm, "' column.")
-                         )
-        if ( !(is.character(object[[nm]])) )
-            errors  <- c(errors,
-                         paste0("Column '", nm, "' should have type character.")
-                         )
-    }
+    if (!all(colnames(object)==
+             c("name", "kind", "subproblem") ) )
+        errors  <- c(errors,
+                     'Columns should be:\n\t c("name", "kind", "subproblem")')
+    if (!all(sapply(object, is.character)==TRUE))
+        errors  <- c(errors,
+                     'All columns should have type character.')
     if ( !all(object[['kind']] %in% c("treatment", "control")) )
         errors  <- c(errors,
                      "'kind' values other than 'treatment' or 'control'.")
+    
     if (length(errors)==0) TRUE else errors  
 })
+
+setClass("MCFSolutions", representation(subproblems='SubProbInfo',nodes='NodeInfo',
+                                        arcs='ArcInfo',matchables="MatchablesInfo"))
--- tests/testthat/test.MCFSolutions.R
+++ tests/testthat/test.MCFSolutions.R
@@ -1,15 +1,60 @@
 context("MCFSolutions & co: S4 classes to encode min-cost-flow solutions")
 
 test_that("Instantiation & validity", {
-    mbls1  <- new("MatchablesInfo", data.frame(name=c('a','b'), subproblem=rep("c",2),
-                                              'kind'=c('treatment', 'control'), stringsAsFactors=F))
-    expect_true(validObject(mbls1))
+    expect_silent(spi1  <- new("SubProbInfo", data.frame(subproblem=c('a','b'), hashed_dist=c('a','b'),
+                                           resolution=c(1,10), exceedance=c(.5, 2), CS_orig_dist=c(TRUE,FALSE),
+                                           stringsAsFactors=F)
+                 ) )
+    expect_true(validObject(spi1))
+
+    spi2  <- spi1
+    colnames(spi2)[1]  <- "Subprob"
+    expect_error(validObject(spi2), "Cols 1-5 should be")
+
+    expect_silent(ni  <- new("NodeInfo",
+                             data.frame(name='a', price=0.5, kind='bookkeeping',
+                                        supply=1L, subproblem='b',
+                                        stringsAsFactors=F)
+                             )
+                  )
+    expect_error(new("NodeInfo",
+                      data.frame(name='a', price=5L, kind='bookkeeping',
+                                 supply=1L, subproblem='b',
+                                 stringsAsFactors=F)
+                     ),
+                 "should have type double" # Not sure it's necessary, but insisting
+                  )                        # that 'price' be double not integer
+
+    expect_silent(ai  <- new("ArcInfo",
+                             matches=data.frame(subproblem='a', treatment='b',
+                                                control=c('c','d'),stringsAsFactors=F),
+                             bookkeeping=data.frame(subproblem='a', startnode=c('c','d'),
+                                                    endnode='(_Sink_)', flow=1L, stringsAsFactors=F)
+                             )
+                  )
+    expect_error(new("ArcInfo",
+                     matches=data.frame(subproblem='a', treatment='b',
+                                        control=c('c','d'),stringsAsFactors=F),
+                     bookkeeping=data.frame(subproblem='a', startnode=c('c','d'),
+                                            endnode='(_Sink_)', flow=1.5, stringsAsFactors=F)
+                     ), "should have type integer" # Not sure it's necessary, but insisting 
+                  )                                # that 'flow' be integer not double
+    
+    expect_silent(mbls1  <- new("MatchablesInfo",
+                                data.frame(name=c('a','b'), 'kind'=c('treatment', 'control'),
+                                           subproblem=rep("c",2), stringsAsFactors=F)
+                                )
+                  )
 
     mbls2  <- mbls1
     colnames(mbls2)[3]  <- "Kind"
-    expect_error(validObject(mbls2), "eed a 'kind' column", fixed=TRUE)
+    expect_error(validObject(mbls2), "Columns should be", fixed=TRUE)
 
     mbls3  <- mbls1
-    mbls3@.Data[[3]]  <- rep("Exposed", 2)
+    mbls3@.Data[[2]][1]  <- "Exposed"
     expect_error(validObject(mbls3), "values other than 'treatment' or 'control'", fixed=TRUE)
+
+    expect_silent(new("MCFSolutions", subproblems=spi1,nodes=ni,arcs=ai,matchables=mbls1))
+    expect_silent(mcf2  <- new("MCFSolutions", subproblems=spi1,nodes=ni,arcs=ai,matchables=mbls2))
+    expect_error(validObject(mcf2, complete=TRUE), "Columns should be", fixed=TRUE)
 })
