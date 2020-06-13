30addc804c62be5d61672d09325eaeed721cf048
Ben Hansen <bbh@umich.edu>
2020-06-13 13:10:07 -0400

    `edgelist(*,y)` now keeps only the nodes named in `y`

--- R/edgelist.R
+++ R/edgelist.R
@@ -5,9 +5,11 @@
 setGeneric("edgelist", function(x, y) { stop("Not implemented.") })
 
 setMethod("edgelist", c(x = "InfinitySparseMatrix"), function(x, y) {
-    return(data.frame(i = factor(x@rownames[x@rows], levels=y),
+    el  <- data.frame(i = factor(x@rownames[x@rows], levels=y),
                       j = factor(x@colnames[x@cols], levels=y),
-                      dist = x@.Data))
+                      dist = x@.Data)
+    ccs  <- complete.cases(el) # to remove rows involving i/j not in y
+    el[ccs,]
 })
 
 setMethod("edgelist", c(x = "matrix"), function(x, y) {
@@ -17,7 +19,9 @@ setMethod("edgelist", c(x = "matrix"), function(x, y) {
 setMethod("edgelist", c(x = "data.frame"), function(x, y){
     stopifnot(ncol(x)==3, setequal(colnames(x), c('control', 'treated', 'distance')),
               is.numeric(x$distance))
-    data.frame(i = factor(x$treated, levels=y),
-               j= factor(x$control, levels=y),
-               dist=x$distance)
+    el <- data.frame(i = factor(x$treated, levels=y),
+                     j= factor(x$control, levels=y),
+                     dist=x$distance)
+    ccs  <- complete.cases(el) # to remove rows involving i/j not in y
+    el[ccs,]    
     })
--- tests/testthat/test.edgelist.R
+++ tests/testthat/test.edgelist.R
@@ -9,14 +9,27 @@ test_that("Basic", {
                                     rows = c(2, 2, 3, 3, 3, 5, 5),
                                     colnames = cns,
                                     rownames = rns)
-    ism.el <- edgelist(ism)
+    ism.el <- edgelist(ism, c(rns, cns))
 
     expect_equal(filter(ism.el, i == "TU" & j == "C3")$dist, 7)
 
 
     m <- as.matrix(ism)
-    m.el <- edgelist(m)
+    m.el <- edgelist(m, c(rns, cns))
 
     expect_equal(filter(m.el, i == "TX" & j == "C3")$dist, 5)
 })
 
+test_that("remove edges to or from nodes not appearing in y",{
+    library(dplyr)
+    cns <- c("C1", "C2", "C3", "C4")
+    rns <- c("TZ", "TY", "TX", "TW", "TU")
+    ism <- makeInfinitySparseMatrix(1:7,
+                                    cols = c(1, 2, 1, 2, 3, 1, 3),
+                                    rows = c(2, 2, 3, 3, 3, 5, 5),
+                                    colnames = cns,
+                                    rownames = rns)
+    ism.el <- edgelist(ism, c(rns[-2], cns))
+    expect_equal(nrow(ism.el), length(ism)-2)
+    expect_equal(filter(ism.el, i == "TU" & j == "C3")$dist, 7)
+})
