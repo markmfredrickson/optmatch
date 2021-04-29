a367935a5c497faaf696441c1bfab9f34979570a
Josh Errickson <josh@errickson.net>
2021-04-29 19:34:58 -0400

    Implementing [ subsetting of ISM objects #63

--- R/InfinitySparseMatrix.R
+++ R/InfinitySparseMatrix.R
@@ -350,6 +350,7 @@ function(e1, e2) {
 ##' @param ... Other arguments are ignored.
 ##' @return An InfinitySparseMatrix with only the selected elements.
 ##' @author Mark Fredrickson
+##' @rdname ism.subset
 ##' @export
 subset.InfinitySparseMatrix <- function(x, subset, select, ...) {
 
@@ -392,6 +393,48 @@ discardOthers <- function(x, index) {
 
   return(y)
 }
+
+##' @param i Row indices.
+##' @param j Col indices.
+##' @param drop Ignored.
+##' @rdname ism.subset
+##' @export
+setMethod("[", "InfinitySparseMatrix",
+          function(x, i, j =NULL, ..., drop = TRUE) {
+            if ( "drop" %in% names(match.call())) {
+              warning("'drop' argument ignored for InfinitySparseMatrix object subsetting ")
+            }
+            # Handles [X] cases
+            if (nargs() < 3) {
+              if (missing(i)) {
+                return(x)
+              }
+              return(x@.Data[i, ...])
+            } else {
+              # At this point we have two arguments, but one could be
+              # null (e.g. [X,] or [,X], as opposed to [X])
+
+              # when missing, replace with NULL to be handled below
+              if (missing(i)) i <- NULL
+              if (missing(j)) j <- NULL
+
+              makelogical <- function(index, rowcol) {
+                switch(class(index),
+                       "numeric" = (1:dim(x)[rowcol]) %in% index,
+                       "integer" = (1:dim(x)[rowcol]) %in% index,
+                       "character" = dimnames(x)[[rowcol]] %in% index,
+                       "logical" = index,
+                       "NULL" = rep(TRUE, dim(x)[rowcol]),
+                       stop("Unrecognized class"))
+              }
+
+              subi <- makelogical(i, 1)
+              subj <- makelogical(j, 2)
+
+              subset(x, subset=subi, select=subj)
+            }
+          })
+
 
 
 ##' This matches the syntax and semantics of
--- tests/testthat/test.InfinitySparseMatrix.R
+++ tests/testthat/test.InfinitySparseMatrix.R
@@ -599,3 +599,53 @@ test_that("rbinds involving BISMs", {
     expect_true(all(rownames(rbind(bismA, bismB)) %in% c(2, 3, 5, 6)))
     expect_true(all(colnames(cbind(t(bismA),t(bismB))) %in% c(2, 3, 5, 6)))
 })
+
+test_that("ISM indexing", {
+
+  data(nuclearplants)
+  m <- match_on(pr ~ cost, data = nuclearplants, caliper = 1)
+
+  # [X, X]
+  expect_equal(dim(m[1:3,2:3]), c(3,2))
+  expect_equal(dim(m[3:2,4:2]), c(2,3))
+  expect_equal(dim(m[c("A", "C"), c(4,7,1,2:4)]), c(2, 5))
+
+  # [X]
+  expect_equal(length(m[1:3]), 3)
+  expect_equal(length(m[c("A", "a")]), 2)
+
+  # [X,] or [,X]
+  expect_equal(dim(m[1:3, ]), c(3, 22))
+  expect_equal(dim(m[, 5:3]), c(10, 3))
+
+  # []
+  expect_equal(m, m[])
+
+  # [,]
+  m2 <- m[,]
+  m@call <- NULL
+  m2@call <- NULL
+  expect_equal(m, m2)
+
+  # Warning whenever `drop` is presented.
+  expect_warning(m[1:3, 1:3, drop = TRUE])
+  expect_warning(m[1:3, 1:3, drop = FALSE])
+  expect_warning(m[1:3,, drop = FALSE])
+  expect_warning(m[1:3, drop = FALSE])
+
+  # Ignoring drop
+  expect_warning({
+    expect_equal(m[1:3, 2:3, drop = TRUE ], m[1:3, 2:3])
+    expect_equal(m[1:3, 2:3, drop = FALSE], m[1:3, 2:3])
+    expect_equal(m[1:3, , drop = TRUE ], m[1:3, ])
+    expect_equal(m[1:3, , drop = FALSE], m[1:3, ])
+    expect_equal(m[, 1:3, drop = TRUE ], m[, 1:3])
+    expect_equal(m[, 1:3, drop = FALSE], m[, 1:3])
+    expect_equal(m[, , drop = TRUE ], m[, ])
+    expect_equal(m[, , drop = FALSE], m[, ])
+    expect_equal(m[, drop = TRUE ], m[, ])
+    expect_equal(m[, drop = FALSE], m[, ])
+    expect_equal(m[drop = TRUE ], m[])
+    expect_equal(m[drop = FALSE], m[])
+  })
+})
