b5cc12168d6b3a9b745b49dc2c08f107e55bd484
Josh Errickson <josh@errickson.net>
2023-02-06 11:48:11 -0500

    Protection against CBPS not being found

--- tests/testthat/test.CBPS.R
+++ tests/testthat/test.CBPS.R
@@ -1,8 +1,8 @@
 context("CBPS integration")
 
 test_that("internal predict.CBPS function", {
-  if (require('CBPS')) {
-    cpsm <- CBPS(pr ~ date + t1 + t2 + cap + ne + ct + bw + cum.n, ATT = 0, data = nuclearplants)
+  if (requireNamespace('CBPS', quietly = TRUE)) {
+    cpsm <- CBPS::CBPS(pr ~ date + t1 + t2 + cap + ne + ct + bw + cum.n, ATT = 0, data = nuclearplants)
     expect_true(is(fullmatch(cpsm, data = nuclearplants), "optmatch"))
 
     # Check recognition of type argument.
